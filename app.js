ae466a91fde18dd93a1a8535c38afe2c6f71e9ce
imshukla10 <ritesh.1923ec1099@kiet.edu>
2023-01-02 17:06:28 +0530

    First

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.set('strictQuery', false);
+// in place of 127.0.0.1 we can also use localhost but localhost cause problems relalted to collection 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+
+//Database Schema
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+  
+//Database encryption must be added before creating model of database.
+// const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+
+//Database model
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+//new User register
+app.post("/register",function(req,res){
+    //creating object of new user
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    //saving newUser object into database
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        }
+        else{
+            console.log(err);
+        }
+    })
+});
+
+//login route
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else if(foundUser && foundUser.password === password){
+            res.render("secrets");
+        }
+        else{
+            console.log("Error 404");
+        }
+     });
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
