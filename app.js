f350af3345909fd41a552658b7f2109da67e5a77
imshukla10 <ritesh.1923ec1099@kiet.edu>
2023-01-03 16:07:25 +0530

    "OAUTH Google"

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+//Passport google oauth20
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//mongoose findOrCreate
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -37,11 +41,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
 //Database Schema
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    secret:String
 });
   
 //passport local mongoose 
 userSchema.plugin(passportLocalMongoose);
+//mongoose findOrCreate
+userSchema.plugin(findOrCreate);
 
 //Database encryption must be added before creating model of database.
 
@@ -49,12 +57,49 @@ userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID:process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ username: profile.emails[0].value, googleId: profile.id, }, function (err, user) {
+        return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     res.render("home");
 });
+
+//register and login using google
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile","email"] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google",{failureRedirect:"/login"}),
+    function(req,res){
+        res.redirect("/secrets");
+    }
+);
 
 app.get("/login",function(req,res){
     res.render("login");
@@ -65,13 +110,43 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
-    //checking if the user is authentic then it will access this page
+    User.find({"secret":{$ne:null}},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                res.render("secrets",{userWithSecrets:foundUser});
+            }
+        }
+    });
+});
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
     } 
+});
+
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 app.get("/logout",function(req,res){
