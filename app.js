ff1a20d1650bd69de1af12eddd5cc176a736dbcd
imshukla10 <ritesh.1923ec1099@kiet.edu>
2023-01-03 14:36:18 +0530

    passport encryption

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
+// npm i passport passport-local passport-local-mongoose express-session for cookies
+//passport local mongoose hash and salt our password automatically
 //jshint esversion:6
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 10;
+//encrypt using passport 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+//session 
+app.use(session({
+    secret:"Our little secret.",
+    resave:false,
+    saveUninitialized:false
+}));
+
+//passport
+app.use(passport.initialize());
+//tell passport to use session
+app.use(passport.session());
+
 
 mongoose.set('strictQuery', false);
 // in place of 127.0.0.1 we can also use localhost but localhost cause problems relalted to collection 
@@ -25,13 +40,17 @@ const userSchema = new mongoose.Schema({
     password:String
 });
   
+//passport local mongoose 
+userSchema.plugin(passportLocalMongoose);
+
 //Database encryption must be added before creating model of database.
-
-
 
 //Database model
 const User = mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
@@ -45,45 +64,58 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+app.get("/secrets",function(req,res){
+    //checking if the user is authentic then it will access this page
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    } 
+});
+
+app.get("/logout",function(req,res){
+    req.logout(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+});
+
 //new User register
 app.post("/register",function(req,res){
-//bcrypt hashing {password,saltRound,callback method}
-    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
-        //creating object of new user
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        //saving newUser object into database
-        newUser.save(function(err){
-            if(!err){
-                res.render("secrets");
-            }
-            else{
-                console.log(err);
-            }
-        });
+    User.register({username:req.body.username},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
     });
 });
 
 //login route
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email:username},function(err,foundUser){
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    req.logIn(user,function(err){
         if(err){
-            console.log(err);
+            res.render("login");
         }
         else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err,result){
-                    if(result === true){
-                        res.render("secrets");
-                    }
-                });
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
-     });
+    });
+
 });
 
 app.listen(3000,function(){
